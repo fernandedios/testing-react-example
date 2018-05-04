ca6718f58cdca1cd3d58b19b2b3992dd6a102cd1
Fernan de Dios <info@mediagramstudios.com>
2018-05-04 08:42:00 +0800

    added initial app test in enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders the React logo image', () => {
+    const app = shallow(<App />);
+    expect(app.find('img.App-logo').length).toEqual(1);
+  });
+
+  it('renders h1 welcome message', () => {
+    const app = shallow(<App />);
+    expect(app.find('h1.App-title').text()).toEqual('Welcome to React');
+  });
 });
