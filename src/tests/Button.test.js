a58149c45d5aedb8cbdcfcccf815eba40d64607e
Fernan de Dios <info@mediagramstudios.com>
2018-05-05 08:29:06 +0800

    added snapshot and class  tests

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import ReactTestRenderer from 'react-test-renderer';
+
 import { Button } from '../components';
 
 describe('<Button />', () => {
@@ -16,9 +18,21 @@ describe('<Button />', () => {
     expect(output.prop('name')).toEqual('test');
   });
 
+  it('should have the correct classNames', () => {
+    const output = shallow(<Button name="test" />);
+
+    expect(output.find('button').hasClass('btn')).toEqual(true);
+    expect(output.find('button').hasClass('btn-primary')).toEqual(true);
+  });
+
   it('should call mockFunction when clicked', () => {
     const output = shallow(<Button name="test" handleClick={mockFunction} />);
     output.simulate('click');
     expect(mockFunction).toHaveBeenCalled();
+  });
+
+  it('should render correctly', () => {
+    const output = ReactTestRenderer.create(<Button name="test" />).toJSON();
+    expect(output).toMatchSnapshot();
   });
 });
