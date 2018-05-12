e0076ad263041a991b3f2673711f19262c038add
Fernan de Dios <info@mediagramstudios.com>
2018-05-12 13:28:08 +0800

    added a few more WithConditional tests

--- src/tests/WithConditional.test.js
+++ src/tests/WithConditional.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import ReactTestRenderer from 'react-test-renderer';
 
 import { Greeter } from '../components';
 import WithConditional from '../hocs/WithConditional';
@@ -13,5 +14,24 @@ describe('<WithConditional />', () => {
     const ConditionalComponent = WithConditional(Greeter);
     const output = shallow(<ConditionalComponent condition={true} />);
     expect(output.html()).not.toBe(null);
+  });
+
+  it('should NOT render if condition evaluates to false', () => {
+    const ConditionalComponent = WithConditional(Greeter);
+    const output = shallow(<ConditionalComponent condition={false} />);
+    expect(output.html()).toBe(null);
+  });
+
+  it('should pass through additional props', () => {
+    const name = 'Fernan';
+    const ConditionalComponent = WithConditional(Greeter);
+    const output = shallow(<ConditionalComponent name={name} condition={true} />);
+    expect(output.html()).toEqual(`<div>Hello ${name}!</div>`);
+  });
+
+  it('should render correctly', () => {
+    const ConditionalComponent = WithConditional(Greeter);
+    const output = ReactTestRenderer.create(<ConditionalComponent condition={true} />).toJSON();
+    expect(output).toMatchSnapshot();
   });
 });
